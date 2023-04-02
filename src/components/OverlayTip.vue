f5df31a15379ac8d2f49865dc69d9e61b137a9b6
OwOmelon <96761586+OwOmelon@users.noreply.github.com>
2023-04-02 14:56:06 +0800

    Add files via upload

---
+++ src/components/OverlayTip.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+	<div
+		class="grid place-items-center w-[70%] mx-auto text-xs md:text-sm text-center"
+	>
+		when using a filter overlay, make sure to put it above the main texture
+		pack
+
+		<Icon icon="tabler:arrow-narrow-down" class="w-10 h-10 my-2" />
+
+		<img
+			src="../assets/overlaytip.png"
+			class="border-[1px] border-white/25 rounded-lg"
+		/>
+	</div>
+</template>
